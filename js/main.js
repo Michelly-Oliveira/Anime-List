31337bfd29d7ca82edcd1e7a2eea3176d93954ce
Michelly-Oliveira <michellyneris.oliveira@gmail.com>
2020-05-07 11:06:47 -0300

    make thingsbigger on a big screen; show most recent clicked page, function to hide/show elements

--- js/main.js
+++ js/main.js
@@ -99,7 +99,7 @@ query($id: Int){
     genres
     averageScore
     coverImage {
-      large
+	  extraLarge
     }
   }
 }
@@ -162,20 +162,21 @@ function queryAPI(
 function displayAllAnime(animeData) {
 	// Store the anime information from the API or from localStorage
 	let animeArray;
+	let hasNextPage;
 
 	if (animeData.data != undefined) {
 		// Show all animes we got from the API
 		animeArray = animeData.data.Page.media;
+		// See if the page we queried from the api has a next page
+		hasNextPage = animeData.data.Page.pageInfo.hasNextPage;
 	} else {
 		// Show the animes saved as favorites on localStorage
 		animeArray = animeData;
+		// Since we are on the anime-fav-page, there is no next page
+		hasNextPage = false;
 	}
 
-	if (animeData.data.Page.pageInfo.hasNextPage) {
-		showMoreBtn.classList.remove('hide');
-	} else {
-		showMoreBtn.classList.add('hide');
-	}
+	displayElement(showMoreBtn, hasNextPage);
 
 	// Add content to the screen as a string of divs, each div containing an anime image and name, with the id associate to it
 	// container += animeArray : if the container already has content, don't clear it, add after the content
@@ -215,7 +216,7 @@ function displayOneAnime(animeData) {
 			<i class="fa fa-times"></i>
 		</button>
 		<div class="img-favBtn">
-			<img src="${anime.coverImage.large}" alt="cover">
+			<img src="${anime.coverImage.extraLarge}" alt="cover">
 			<button class="favBtn no-outline cursor">${buttonText}</button>
 		</div>
 		<div class="info">
@@ -235,10 +236,10 @@ function displayOneAnime(animeData) {
 	});
 	document.querySelector('#closeBtn').addEventListener('click', closeAnimePage);
 
-	// Show anime page and hide the animes page and showMore btn
-	animePage.classList.add('active');
-	container.classList.add('hide');
-	showMoreBtn.classList.add('hide');
+	// Show anime page, hide the animes page and display showMore btn
+	displayElement(animePage, true);
+	displayElement(container, false);
+	displayElement(showMoreBtn, false);
 
 	// Start at the top of the page
 	window.scrollTo(0, 0);
@@ -325,12 +326,16 @@ function closeAnimePage() {
 	// Clear the anime page
 	animePage.innerHTML = '';
 
-	animePage.classList.remove('active');
-	container.classList.remove('hide');
+	// Hide anime-page
+	displayElement(animePage, false);
+	// Show animes container
+	displayElement(container, true);
 
 	// If the previous page is the fav animes page don't show the ShowMore btn
 	if (favPageBtn.firstElementChild.matches('.fa-heart')) {
-		showMoreBtn.classList.add('hide');
+		displayElement(showMoreBtn, false);
+	} else {
+		displayElement(showMoreBtn, true);
 	}
 
 	// Go back to where we were before opening an anime
@@ -368,6 +373,7 @@ function filterBySortType(e) {
 	variablesFoQuery.sort = sort;
 
 	clearSearchInput();
+	closeAnimePage();
 	refreshScreen();
 }
 
@@ -377,18 +383,19 @@ function filterByGenre(e) {
 	if (genre == 'all') {
 		variablesFoQuery = {
 			page: 1,
-			sort: sort,
+			sort,
 		};
 	} else {
 		// If we choose any other genre, add it to the variables tha will be passed to the query api
 		variablesFoQuery = {
 			page: 1,
-			sort: sort,
-			genre: genre,
+			sort,
+			genre,
 		};
 	}
 
 	clearSearchInput();
+	closeAnimePage();
 	refreshScreen();
 }
 
@@ -421,38 +428,43 @@ function deleteSearch() {
 		sort: 'TRENDING_DESC',
 	};
 
-	// Go back to the first option of the sortField = 'TRENDING_DESC'
-	sortField.selectedIndex = 0;
-	// Go back to the first option of the genreField = 'All'
-	genresField.selectedIndex = 0;
-
+	resetFilters();
+	closeAnimePage();
 	refreshScreen();
 }
 
 function submitSearch() {
 	variablesFoQuery = {
 		page: 1,
-		sort: sort,
+		sort: 'TRENDING_DESC',
 		search: inputAnimeName,
 	};
 
+	// Need to call these functions here so it works when click the search btn
+	resetFilters();
+	closeAnimePage();
 	refreshScreen();
+}
+
+function resetFilters() {
+	// Go back to the first option of the sortField = 'TRENDING_DESC'
+	sortField.selectedIndex = 0;
+	// Go back to the first option of the genreField = 'All'
+	genresField.selectedIndex = 0;
 }
 
 function toggleFavPage(e) {
 	// Check if we clicked on the heart icon inside the button
 	const btn = e.target;
+	closeAnimePage();
 
 	// If heart is full we are on the fav page
 	if (btn.matches('.fa-heart')) {
 		// Make heart empty
 		btn.className = 'fa fa-heart-o';
 		// go back to the animes container page
-		container.classList.remove('anime-fav-page');
 		// Clear the screen and query the API for the animes
 		refreshScreen();
-		// Show
-		showMoreBtn.classList.remove('hide');
 	} else if (btn.matches('.fa-heart-o')) {
 		// If heart is empty we are on the animes container page
 		// Make heart full
@@ -460,8 +472,6 @@ function toggleFavPage(e) {
 		// Clear the screen
 		container.innerHTML = '';
 		// go to the fav anime page
-		container.classList.add('anime-fav-page');
-		showMoreBtn.classList.add('hide');
 		displayAllAnime(animeStorage);
 	}
 }
@@ -473,4 +483,12 @@ function refreshScreen() {
 	variablesFoQuery.page = 1;
 	// Show anime
 	queryAPI();
+}
+
+function displayElement(element, showElement) {
+	if (showElement) {
+		element.classList.remove('hide');
+	} else {
+		element.classList.add('hide');
+	}
 }
